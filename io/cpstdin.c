7da5129a7bca7c42a26f039f9c28f47f5d490008
amychoi7@AmyDell <amychoi7@snu.ac.kr>
2024-04-24 22:01:16 +0900

    io

--- io/cpstdin.c
+++ io/cpstdin.c
@@ -1,4 +1,8 @@
 #include "../csapp.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
 
 ssize_t Read(int fd, void *buf, size_t count) {
     return read(fd, buf, count);
@@ -8,9 +12,17 @@ ssize_t Write(int fd, const void *buf, size_t count){
 }
 
 int main (int argc, char* argv[]) {
+    // cpstdin
     char c;
     while (Read (STDIN_FILENO, &c, 1) != 0) {
         Write(STDOUT_FILENO, &c, 1);
     }
+
+    // myDummy
+    // const char * text = "hi, my name is Amy\n";
+    // int dummy_fd = open ("dummy.txt", O_WRONLY | O_CREAT | O_APPEND, S_IRUSR);
+    // write(dummy_fd, text, strlen(text));
+    // close(dummy_fd);
+
     exit(0);
 }
